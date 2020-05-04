7a8df5624531fa31d5539a6980648ff1b9ab64f3
Audin <audin@diamondonline.com.tw>
2020-05-04 16:23:32 +0800

    新增專案 WebApi

---
+++ WebApi_1/Protocol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_1
+{
+    public enum Food
+    {
+        Candy,
+        Cookie,
+        Cake
+    }
+}
